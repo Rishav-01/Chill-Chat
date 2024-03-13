1454c4626c2540e32bf64766c7e8374474b13cb7
Rishav <rishulal29@gmail.com>
2024-03-13 14:56:14 +0530

    fixes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateTokenAndSetCookie = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
-    sameSite: "strict",
-    httpOnly: true,
+    sameSite: "None",
+    secure: true,
   });
 };
 
